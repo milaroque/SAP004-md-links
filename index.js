1d685d67726a17d2f22237416fa4b38eb93162e5
Camila Roque <milaroque@icloud.com>
2020-07-04 12:08:23 -0300

    funçao mdlinks

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+
+const mdLinks = (file) => {
+  const array = [];
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        reject(err.message);
+      } else {
+        const regex = /\[([^\[]+)\]\((.*)\)/gm;
+        const links = data.match(regex);
+        links.forEach((elem) => {
+          text = elem.match(/\[([^\[]+)\]/)[1].replace('\n', '');
+          href = elem.match(/\((.*)\)/)[1];
+          array.push({ text, href, file });
+        })
+        resolve(array);
+      }
+    })
+  })
+}
+module.exports = mdLinks;
