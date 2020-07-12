eaa07d72f7084c8fa3dc0efc95ceaa43b344eb2b
Camila Roque <milaroque@icloud.com>
2020-07-11 23:51:18 -0300

    desgraça de teste

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -5,42 +5,34 @@ describe('mdLinks', () => {
   it('mdLinks should be a function', () => {
     expect(typeof mdLinks).toBe('function');
   });
-
-  it('should return validation', (done) => {
+  it('should return validation', () => {
     mdLinks('./test/', '--validate').then((result) => {
       expect(result).toEqual(mock.arrayValidate);
-      done();
     });
   });
-  it('should return validation', (done) => {
+  it('should return validation', () => {
     mdLinks('./test/test.md', '--validate').then((result) => {
       expect(result).toEqual(mock.arrayValidate);
-      done();
     });
   });
-  it('should return validation and stats', (done) => {
+  it('should return validation and stats', () => {
     mdLinks('./test/test.md', '--validate', '--stats').then((result) => {
       expect(result).toEqual(mock.arrayValidate, mock.arrayStats);
-      done();
     });
   });
-  it('should return array objects', (done) => {
+  it('should return array objects', () => {
     mdLinks('./test/test.md').then((result) => {
       expect(result).toEqual(mock.array);
-      done();
     });
   });
-  it('should return stats', (done) => {
-    mdLinks('./test/', '--stats').then((result) => {
-      expect(result).toEqual(mock.arrayStats);
-      done();
-    });
+  it('should return stats', async () => {
+    const data = await mdLinks('./test/', '--stats');
+    expect(data).toEqual(mock.arrayStats);
   });
-  it('erro', (done) => {
+  it('erro', () => {
     mdLinks('./testsss/test.md')
       .catch((err) => {
         expect(err).toBe('Sorry, but there is no archive with that name in this directory');
-        done();
       });
   });
 });
