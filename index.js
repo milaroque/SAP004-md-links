4c236fa5bf79da65fea7aa87040a1e68e6f0ad68
Camila Roque <milaroque@icloud.com>
2020-08-19 20:27:54 -0300

    att

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const readPath = require('./readdir.js');
-const readFile = require('./readfile.js');
+const readPath = require('./src/readdir.js');
+const readFile = require('./src/readfile.js');
 
 const mdLinks = (file, option = []) => new Promise((resolved, rejected) => {
   fs.stat(file, (err, stats) => {
